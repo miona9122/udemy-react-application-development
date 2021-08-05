5412401f127d2005b4d1f49ef494c2d11ce90334
miona9122 <lacco242122@gmail.com>
2021-08-06 08:40:24 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from "react";
+
 function App() {
+  // JSXの記述
+  //const dom = <h1 className="foo">Hi, Tom!</h1>;
+  // JavaScriptの記述
+  // return React.createElement("div", null, "Hello, world!!");
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onClick={() => console.log("Hi!")} />
+    </React.Fragment>
   );
 }
 
