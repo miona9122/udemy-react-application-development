1fa174e62f02c664a8afde213f37fc9e06cceb27
miona9122 <lacco242122@gmail.com>
2021-08-06 09:00:18 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 function App() {
-  // JSXの記述
-  //const dom = <h1 className="foo">Hi, Tom!</h1>;
-  // JavaScriptの記述
-  // return React.createElement("div", null, "Hello, world!!");
   return (
     <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={() => console.log("Hi!")} />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </React.Fragment>
   );
 }
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
